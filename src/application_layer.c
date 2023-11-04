808dd9a86a25343af612593649e6e4702699261c
Samuel Maciel <samuel.200322.maciel@gmail.com>
2023-11-04 20:12:12 +0000

    llopen almost done

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,9 +1,18 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    LinkLayer ll;
+    strcpy(ll.serialPort,serialPort);
+    ll.role = strcmp(role, "tx") ? LlRx : LlTx;
+    ll.baudRate = baudRate;
+    ll.nRetransmissions = nTries;
+    ll.timeout = timeout;
+
+
+    llopen(ll);
 }
