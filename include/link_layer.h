b12a289cb48c14dc9635d843f55d48af0d7ffb5c
Samuel Maciel <samuel.200322.maciel@gmail.com>
2023-11-07 01:37:33 +0000

    More Macros

--- include/link_layer.h
+++ include/link_layer.h
@@ -42,13 +42,17 @@ typedef struct
 
 #define SUPFRAME_SIZE 5 //
 
-#define FLAG   0x7E
-#define A_SET  0x03
-#define C_SET  0x03
-#define A_UA   0x01
-#define C_UA   0x07
-#define C_I0   0x00
-#define C_I1   0x40
+#define FLAG    0x7E
+#define A_SET   0x03
+#define C_SET   0x03
+#define A_UA    0x01
+#define C_UA    0x07
+#define C_I0    0x00
+#define C_I1    0x40
+#define C_RR0   0X05
+#define C_RR1   0X85
+#define C_REJ0  0X01
+#define C_REJ1  0X81
 
 // Open a connection using the "port" parameters defined in struct linkLayer.
 // Return "1" on success or "-1" on error.
